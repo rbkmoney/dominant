1007632f37428e8523b911859ef93d16a2884087
Timur <timka@users.noreply.github.com>
2017-10-20 14:27:26 +0300

    We decided to get rid of IPv6 in compose (#54)

--- docker-compose.sh
+++ docker-compose.sh
@@ -19,12 +19,4 @@ services:
       - ./test/machinegun/sys.config:/opt/machinegun/releases/0.1.0/sys.config
     environment:
       - SERVICE_NAME=machinegun
-
-
-networks:
-  default:
-    driver: bridge
-    driver_opts:
-      com.docker.network.enable_ipv6: "true"
-      com.docker.network.bridge.enable_ip_masquerade: "true"
 EOF
