00dc9bbfcfc21f9f018e76e000ba8bb2599c81cb
Andrey Fadeev <me@ciiol.net>
2019-12-04 12:14:51 +0200

    MSPF-517 Add `default_woody_handling_timeout` option (#109)
    The option allow to configure defualt woody deadline for requests without it.

---
+++ src/dmt_api_woody_utils.erl
@@ -0,0 +1,15 @@
+-module(dmt_api_woody_utils).
+
+-export([ensure_woody_deadline_set/2]).
+
+%% API
+
+-spec ensure_woody_deadline_set(woody_context:ctx(), woody_deadline:deadline()) ->
+    woody_context:ctx().
+ensure_woody_deadline_set(WoodyContext, Default) ->
+    case woody_context:get_deadline(WoodyContext) of
+        undefined ->
+            woody_context:set_deadline(Default, WoodyContext);
+        _Other ->
+            WoodyContext
+    end.
