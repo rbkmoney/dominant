ef1c6971cea878b4ce51e186246d3cb983cdf805
Andrey Fadeev <me@ciiol.net>
2020-06-29 10:54:14 +0300

    Add migration from v4 to v5

--- src/dmt_api_repository_migration.erl
+++ src/dmt_api_repository_migration.erl
@@ -11,9 +11,6 @@
     limit   => 20   % 2xBASE, maybe even less
 }).
 
--define(OLD_REPO, dmt_api_repository_v4).
--define(NEW_REPO, dmt_api_repository_v4).
-
 %% API
 
 -export([checkout/2]).
@@ -44,9 +41,9 @@
 checkout(Ref, Context) ->
     case is_migration_finished(Context) of
         true ->
-            ?NEW_REPO:checkout(Ref, Context);
+            dmt_api_repository_v5:checkout(Ref, Context);
         false ->
-            ?OLD_REPO:checkout(Ref, Context)
+            dmt_api_repository_v4:checkout(Ref, Context)
     end.
 
 -spec pull(dmt_api_repository:version(), context()) ->
@@ -63,9 +60,9 @@ pull(Version, Context) ->
 pull(Version, Limit, Context) ->
     case is_migration_finished(Context) of
         true ->
-            ?NEW_REPO:pull(Version, Limit, Context);
+            dmt_api_repository_v5:pull(Version, Limit, Context);
         false ->
-            ?OLD_REPO:pull(Version, Limit, Context)
+            dmt_api_repository_v4:pull(Version, Limit, Context)
     end.
 
 -spec commit(dmt_api_repository:version(), commit(), context()) ->
@@ -75,7 +72,7 @@ pull(Version, Limit, Context) ->
 commit(Version, Commit, Context) ->
     case is_migration_finished(Context) of
         true ->
-            ?NEW_REPO:commit(Version, Commit, Context);
+            dmt_api_repository_v5:commit(Version, Commit, Context);
         false ->
             {error, migration_in_progress}
     end.
@@ -93,7 +90,7 @@ process_call(
     process_call_(Call, Machine, Context);
 process_call(Call, Machine, Context) ->
     % This is for v5 proccessor
-    ?NEW_REPO:process_call(Call, Machine, Context).
+    dmt_api_repository_v5:process_call(Call, Machine, Context).
 
 -spec process_call_(dmt_api_automaton_handler:call(), machine(), context()) -> no_return().
 process_call_(_Call, _Machine, _Context) ->
@@ -112,7 +109,7 @@ process_signal(
     process_signal_(Signal, Machine, Context);
 process_signal(Signal, Machine, Context) ->
     % This is for v5 proccessor
-    ?NEW_REPO:process_signal(Signal, Machine, Context).
+    dmt_api_repository_v5:process_signal(Signal, Machine, Context).
 
 process_signal_({init, #mg_stateproc_InitSignal{}}, _Machine, _Context) ->
     start_migration();
@@ -148,7 +145,7 @@ continue_migration(#{version := Version, is_finished := true} = State, _Context)
 continue_migration(#{version := Version, is_finished := false} = OldState, Context) ->
     Limit = maps:get(limit, get_migration_settings()),
     _ = logger:info(<<"Migrating events from ~p to ~p">>, [Version, Version + Limit]),
-    NewState = case ?OLD_REPO:pull(Version, Limit, Context) of
+    NewState = case dmt_api_repository_v4:pull(Version, Limit, Context) of
         {ok, History} when map_size(History) > 0 ->
             OldState#{version => try_migrate_history(Version, History, Context)};
         {ok, _EmptyHistory} ->
@@ -162,7 +159,7 @@ try_migrate_history(Version, History, Context) ->
     case maps:get(NextVersion, History, undefined) of
         #'Commit'{} = Commit ->
             MigratedCommit = migrate_commit(Commit),
-            {ok, #'Snapshot'{version = NextVersion}} = ?NEW_REPO:commit(Version, MigratedCommit, Context),
+            {ok, #'Snapshot'{version = NextVersion}} = dmt_api_repository_v5:commit(Version, MigratedCommit, Context),
             %% continue history traversing
             try_migrate_history(NextVersion, History, Context);
         undefined ->
@@ -199,17 +196,126 @@ decode_aux_state(1, {obj, #{
     #{version => Version, is_finished => IsFinished}.
 
 migrate_commit(#'Commit'{ops = Ops} = Commit) ->
-    Commit#'Commit'{ops = lists:map(fun migrate_op/1, Ops)}.
+    UpdatedOps = lists:map(fun rewrite_op/1, Ops),
+    NewOps = lists:flatmap(fun add_ops/1, Ops),
+    Commit#'Commit'{ops = UpdatedOps ++ NewOps}.
 
-migrate_op({insert, #'InsertOp'{object = Object} = Op}) ->
-    {insert, Op#'InsertOp'{object = migrate_object(Object)}};
-migrate_op({update, #'UpdateOp'{old_object = OldObject, new_object = NewObject} = Op}) ->
+rewrite_op({insert, #'InsertOp'{object = Object} = Op}) ->
+    {insert, Op#'InsertOp'{object = rewrite_object(Object)}};
+rewrite_op({update, #'UpdateOp'{old_object = OldObject, new_object = NewObject} = Op}) ->
     {update, Op#'UpdateOp'{
-        old_object = migrate_object(OldObject),
-        new_object = migrate_object(NewObject)
+        old_object = rewrite_object(OldObject),
+        new_object = rewrite_object(NewObject)
     }};
-migrate_op({remove, #'RemoveOp'{object = Object} = Op}) ->
-    {remove, Op#'RemoveOp'{object = migrate_object(Object)}}.
+rewrite_op({remove, #'RemoveOp'{object = Object} = Op}) ->
+    {remove, Op#'RemoveOp'{object = rewrite_object(Object)}}.
 
-migrate_object(Object) ->
+rewrite_object({provider, #domain_ProviderObject{data = Data} = Object}) ->
+    NewData = Data#domain_Provider{
+        terms = #domain_ProvisionTermSet{
+            payments = Data#domain_Provider.payment_terms,
+            recurrent_paytools = Data#domain_Provider.recurrent_paytool_terms
+        }
+    },
+    {provider, Object#domain_ProviderObject{data = NewData}};
+rewrite_object({payment_institution, #domain_PaymentInstitutionObject{data = Data} = Object}) ->
+    NewData = Data#domain_PaymentInstitution{
+        withdrawal_providers = rewrite_provider_selector(
+            Data#domain_PaymentInstitution.withdrawal_providers_legacy
+        ),
+        p2p_providers = rewrite_provider_selector(
+            Data#domain_PaymentInstitution.p2p_providers_legacy
+        )
+    },
+    {payment_institution, Object#domain_PaymentInstitutionObject{data = NewData}};
+rewrite_object(Object) ->
     Object.
+
+rewrite_provider_selector({value, Refs}) ->
+    {value, ordsets:from_list(lists:map(fun rewrite_ref/1, Refs))};
+rewrite_provider_selector({decisions, Decisions}) ->
+    {decisions, lists:map(fun rewrite_provider_decision/1, Decisions)}.
+
+rewrite_provider_decision({Name, Predicate, Selector}) ->
+    {Name, Predicate, rewrite_provider_selector(Selector)}.
+
+add_ops({insert, #'InsertOp'{object = Object0} = Op}) ->
+    case maybe_clone_object(Object0) of
+        {add, Object1} ->
+            [{insert, Op#'InsertOp'{object = Object1}}];
+        ignore ->
+            []
+    end;
+add_ops({update, #'UpdateOp'{old_object = OldObject0, new_object = NewObject0} = Op}) ->
+    case maybe_clone_object(OldObject0) of
+        {add, OldObject1} ->
+            {add, NewObject1} = maybe_clone_object(NewObject0),
+            [{update, Op#'UpdateOp'{old_object = OldObject1, new_object = NewObject1}}];
+        ignore ->
+            []
+    end;
+add_ops({remove, #'RemoveOp'{object = Object0} = Op}) ->
+    case maybe_clone_object(Object0) of
+        {add, Object1} ->
+            [{remove, Op#'RemoveOp'{object = Object1}}];
+        ignore ->
+            []
+    end.
+
+maybe_clone_object({withdrawal_provider, Object}) ->
+    #domain_WithdrawalProviderObject{data = Data, ref = Ref} = Object,
+    NewData = #domain_Provider{
+        abs_account = <<"">>,  % remove me
+        name = Data#domain_WithdrawalProvider.name,
+        description = default(Data#domain_WithdrawalProvider.description, <<"">>),
+        proxy = Data#domain_WithdrawalProvider.proxy,
+        identity = Data#domain_WithdrawalProvider.identity,
+        accounts = Data#domain_WithdrawalProvider.accounts,
+        terms = #domain_ProvisionTermSet{
+            wallet = #domain_WalletProvisionTerms{
+                withdrawals = Data#domain_WithdrawalProvider.withdrawal_terms
+            }
+        }
+    },
+    NewRef = rewrite_ref(Ref),
+    {add, {provider, #domain_ProviderObject{data = NewData, ref = NewRef}}};
+maybe_clone_object({p2p_provider, Object}) ->
+    #domain_P2PProviderObject{data = Data, ref = Ref} = Object,
+    NewData = #domain_Provider{
+        name = Data#domain_P2PProvider.name,
+        description = default(Data#domain_P2PProvider.description, <<"">>),
+        proxy = Data#domain_P2PProvider.proxy,
+        identity = Data#domain_P2PProvider.identity,
+        accounts = Data#domain_P2PProvider.accounts,
+        terms = #domain_ProvisionTermSet{
+            wallet = #domain_WalletProvisionTerms{
+                p2p = Data#domain_P2PProvider.p2p_terms
+            }
+        }
+    },
+    NewRef = rewrite_ref(Ref),
+    {add, {provider, #domain_ProviderObject{data = NewData, ref = NewRef}}};
+maybe_clone_object({cash_register_provider, Object}) ->
+    #domain_CashRegisterProviderObject{data = Data, ref = Ref} = Object,
+    NewData = #domain_Provider{
+        name = Data#domain_CashRegisterProvider.name,
+        description = default(Data#domain_CashRegisterProvider.description, <<"">>),
+        proxy = Data#domain_CashRegisterProvider.proxy,
+        params_schema = Data#domain_CashRegisterProvider.params_schema
+    },
+    NewRef = rewrite_ref(Ref),
+    {add, {provider, #domain_ProviderObject{data = NewData, ref = NewRef}}};
+maybe_clone_object(_Object) ->
+    ignore.
+
+rewrite_ref(#domain_WithdrawalProviderRef{id = ID}) ->
+    #domain_ProviderRef{id = ID + 300};
+rewrite_ref(#domain_P2PProviderRef{id = ID}) ->
+    #domain_ProviderRef{id = ID + 400};
+rewrite_ref(#domain_CashRegisterProviderRef{id = ID}) ->
+    #domain_ProviderRef{id = ID + 450}.
+
+default(undefined, Default) ->
+    Default;
+default(Value, _Default) ->
+    Value.
